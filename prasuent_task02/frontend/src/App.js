b9a179fd158493a97e918c21de3c9f4a2c50692c
Rishi Sivaraju <sivarajurishi@gmail.com>
2024-08-13 21:49:01 +0530

    some changes

--- prasuent_task02/frontend/src/App.js
+++ prasuent_task02/frontend/src/App.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import io from 'socket.io-client';
 import './App.css';
 
-const socket = io('http://localhost:3001'); // Replace with your backend URL if different
+const socket = io('http://localhost:3001'); 
 
 function App() {
   const [message, setMessage] = useState('');
